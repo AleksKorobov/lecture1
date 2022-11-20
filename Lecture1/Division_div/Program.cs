d055e2e5c7513420108db1bcd17ec1fcfaa53e1b
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 19:55:02 +0500

    Пример деления

---
+++ Lecture1/Division_div/Program.cs
@@ -0,0 +1,9 @@
+﻿// Деление double позволяет работать с вещественными числами т.е, сможем делить не на целое число в результате.
+//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA/numberB);
+
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA/numberB);
