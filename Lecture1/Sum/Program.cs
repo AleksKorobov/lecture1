34a1784dab2aee60f0669357bd010a44746451c0
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 19:38:46 +0500

    Сумма

---
+++ Lecture1/Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задаем значения первого и второго числа integer - int -
+//1 Вариант
+//int numberA = 2;
+//int numberB = 6;
+//Console.WriteLine(numberA+numberB);
+
+int numberA = 2;
+int numberB = 12;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
+
+
