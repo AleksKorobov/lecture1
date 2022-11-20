1ba1c94e4a175712a9f4c0b214f7b5f147f30d43
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 18:30:18 +0500

    Первый файл

---
+++ Lecture1/Hello_World/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Это Алексей!)");
+//dotnet new console - запуск нового проекта, формируются необходимые папки и файлы
+//dotnet run - запуск написанного в проете кода в работу.
