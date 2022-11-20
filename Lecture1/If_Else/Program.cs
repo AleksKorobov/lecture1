b7b26dbcdbd2a350b26c309e0b719835c27ff8e5
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 20:56:04 +0500

    Привет, Маша!

---
+++ Lecture1/If_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿// Особенное приветствие пользователя
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")//ToLower() -  позволяет все буквы имени пользователя переводить в маленькие и при любом неправильном написани имени МАШа, условие будет работать
+{
+    Console.WriteLine ("Ура, это же МАША!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.Write (username);
+    Console.WriteLine ("!");
+}
