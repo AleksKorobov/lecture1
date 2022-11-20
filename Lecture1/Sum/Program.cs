36fb7a9d103b53a7514e8ea09f6611370ee48330
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 20:27:10 +0500

    Рандомное сложение

--- Lecture1/Sum/Program.cs
+++ Lecture1/Sum/Program.cs
@@ -3,9 +3,17 @@
 //int numberA = 2;
 //int numberB = 6;
 //Console.WriteLine(numberA+numberB);
+//Вариант 2
+//int numberA = 2;
+//int numberB = 12;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+//вариант 3
 
-int numberA = 2;
-int numberB = 12;
+int numberA = new Random().Next(1, 10); // Рандомное число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine (numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
