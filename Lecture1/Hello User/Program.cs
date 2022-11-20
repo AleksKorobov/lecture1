daf539fe05e4e9c088a3a9ee16f63b48827eb19b
Aleks_K <korobov_a_u@mail.ru>
2022-11-20 18:56:05 +0500

    Второй файл готов

---
+++ Lecture1/Hello User/Program.cs
@@ -0,0 +1,5 @@
+﻿// string - строка -, оператор, для присвоения переменной  имени пользователя)
+Console.WriteLine("Введите свое имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
